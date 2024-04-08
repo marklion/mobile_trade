f74d8e1313416e6bf135f412fec93794f457a2ec
marklion <marklion@sina.com>
2024-04-08 08:09:45 +0000

    fix bidding found contract order issue

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 CUR_DIR=$(realpath $(dirname $0))
 pushd ${CUR_DIR}/build
 DC_CON_ID=$(./install.sh)
-docker exec -ti ${DC_CON_ID} bash -c "pip install -r /automation/require.txt ; robot -d /database /automation"
+docker exec -ti ${DC_CON_ID} bash -c 'pip install -r /automation/require.txt ; robot -d /database -v "server_base:http://localhost:8080/api/v1" -v "server_db:/database/mt.db" /automation'
 docker rm -f ${DC_CON_ID}
