152c52ca23f19d0d5f89460d3246ceab0db42b9e
debugerlu <debugerlu@gmail.com>
2024-07-18 16:45:51 +0800

    fixed

--- mt_gui/pages/Login.vue
+++ mt_gui/pages/Login.vue
@@ -16,12 +16,12 @@
         <fui-button text="微信登录" type="success" @click="wx_login"></fui-button>
     </view>
     <view v-else>
-        <fui-form ref="form" top="100" :model="formData" :show="false">
+        <fui-form ref="form" top="100" :model="formData" :show="true" :top="50">
             <fui-form-item label="手机号码" asterisk prop="phone">
                 <fui-input :borderBottom="false" :padding="[0]" placeholder="请输入手机号码" v-model="formData.phone"></fui-input>
             </fui-form-item>
             <fui-form-item label="密码" asterisk prop="password">
-                <fui-input :borderBottom="false" :padding="[0]" password placeholder="请输入密码" v-model="formData.password"></fui-input>
+                <fui-input  :borderBottom="false" :padding="[0]" password placeholder="请输入密码" v-model="formData.password"></fui-input>
             </fui-form-item>
             <fui-button text="登录" bold @click="submit"></fui-button>
         </fui-form>
@@ -39,7 +39,16 @@ export default {
             formData: {
                 phone: '',
                 password: '',
-            }
+            },
+			rules:[{
+				name: "phone",
+				rule: ["required"],
+				msg: ["请输入手机号", "请输入正确的手机号"]
+			}, {
+				name: "password",
+				rule: ["required"],
+				msg: ["请输入密码"]
+			}]
         }
     },
     methods: {
@@ -47,12 +56,20 @@ export default {
             this.cur_tab = e.index;
         },
         submit: async function () {
-            let res = await this.$send_req('/global/pwd_login', this.formData);
-            uni.setStorageSync('token', res.token);
-            await this.$init_self();
-            uni.reLaunch({
-                url: '/pages/Home'
-            });
+			// 客户端登录校验
+			this.$refs.form.validator(this.formData, this.rules).then(async res => {
+					if (res.isPassed) {
+						let res = await this.$send_req('/global/pwd_login', this.formData);
+						uni.setStorageSync('token', res.token);
+						await this.$init_self();
+						uni.reLaunch({
+						    url: '/pages/Home'
+						});
+					}
+				}).catch(err => {
+					console.log(err)
+				})
+            
         },
         wx_login: async function () {
             try {
