89f2be3ec476cee4b05feaedd37bf0cffcc24bf8
debugerlu <debugerlu@gmail.com>
2024-07-22 12:00:37 +0800

    修改rules匹配规则，修改代码缩进

--- mt_gui/pages/Login.vue
+++ mt_gui/pages/Login.vue
@@ -1,32 +1,34 @@
 <template>
-<view>
-    <fui-card full>
-        <view style="display: flex; justify-content: center; flex-direction: column; align-items: center;">
-            <view style="margin-top: 10vh; margin-bottom: 3vh;">
-                <fui-avatar src="/static/logo.jpg" size="large"></fui-avatar>
+    <view>
+        <fui-card full>
+            <view style="display: flex; justify-content: center; flex-direction: column; align-items: center;">
+                <view style="margin-top: 10vh; margin-bottom: 3vh;">
+                    <fui-avatar src="/static/logo.jpg" size="large"></fui-avatar>
+                </view>
+                <view style="font-size:30px;">
+                    掌易助理
+                </view>
             </view>
-            <view style="font-size:30px;">
-                掌易助理
-            </view>
-        </view>
 
-    </fui-card>
-    <fui-tabs :tabs="tabs" @change="change"></fui-tabs>
-    <view v-if="cur_tab == 0" style="margin-top: 10vh">
-        <fui-button text="微信登录" type="success" @click="wx_login"></fui-button>
+        </fui-card>
+        <fui-tabs :tabs="tabs" @change="change"></fui-tabs>
+        <view v-if="cur_tab == 0" style="margin-top: 10vh">
+            <fui-button text="微信登录" type="success" @click="wx_login"></fui-button>
+        </view>
+        <view v-else>
+            <fui-form ref="form" top="100" :model="formData" :show="true" :top="50">
+                <fui-form-item label="手机号码" asterisk prop="phone">
+                    <fui-input maxlength="11" :borderBottom="false" :padding="[0]" placeholder="请输入手机号码"
+                        v-model="formData.phone"></fui-input>
+                </fui-form-item>
+                <fui-form-item label="密码" asterisk prop="password">
+                    <fui-input :borderBottom="false" :padding="[0]" password placeholder="请输入密码"
+                        v-model="formData.password"></fui-input>
+                </fui-form-item>
+                <fui-button text="登录" bold @click="submit"></fui-button>
+            </fui-form>
+        </view>
     </view>
-    <view v-else>
-        <fui-form ref="form" top="100" :model="formData" :show="true" :top="50">
-            <fui-form-item label="手机号码" asterisk prop="phone">
-                <fui-input :borderBottom="false" :padding="[0]" placeholder="请输入手机号码" v-model="formData.phone"></fui-input>
-            </fui-form-item>
-            <fui-form-item label="密码" asterisk prop="password">
-                <fui-input  :borderBottom="false" :padding="[0]" password placeholder="请输入密码" v-model="formData.password"></fui-input>
-            </fui-form-item>
-            <fui-button text="登录" bold @click="submit"></fui-button>
-        </fui-form>
-    </view>
-</view>
 </template>
 
 <script>
@@ -40,15 +42,15 @@ export default {
                 phone: '',
                 password: '',
             },
-			rules:[{
-				name: "phone",
-				rule: ["required"],
-				msg: ["请输入手机号", "请输入正确的手机号"]
-			}, {
-				name: "password",
-				rule: ["required"],
-				msg: ["请输入密码"]
-			}]
+            rules: [{
+                name: "phone",
+                rule: ["required","isNumber"],
+                msg: ["请输入手机号","请输入有效的手机号"]
+            }, {
+                name: "password",
+                rule: ["required"],
+                msg: ["请输入密码"]
+            }]
         }
     },
     methods: {
@@ -56,20 +58,20 @@ export default {
             this.cur_tab = e.index;
         },
         submit: async function () {
-			// 客户端登录校验
-			this.$refs.form.validator(this.formData, this.rules).then(async res => {
-					if (res.isPassed) {
-						let res = await this.$send_req('/global/pwd_login', this.formData);
-						uni.setStorageSync('token', res.token);
-						await this.$init_self();
-						uni.reLaunch({
-						    url: '/pages/Home'
-						});
-					}
-				}).catch(err => {
-					console.log(err)
-				})
-            
+            // 客户端登录校验
+            this.$refs.form.validator(this.formData, this.rules).then(async res => {
+                if (res.isPassed) {
+                    let res = await this.$send_req('/global/pwd_login', this.formData);
+                    uni.setStorageSync('token', res.token);
+                    await this.$init_self();
+                    uni.reLaunch({
+                        url: '/pages/Home'
+                    });
+                }
+            }).catch(err => {
+                console.log(err)
+            })
+
         },
         wx_login: async function () {
             try {
@@ -94,6 +96,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
