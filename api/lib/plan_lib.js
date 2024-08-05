c5cafee682ced0e57573520f1d9047c2a4f67764
debugerlu <debugerlu@gmail.com>
2024-08-05 16:16:18 +0800

    fix

--- api/lib/plan_lib.js
+++ api/lib/plan_lib.js
@@ -6,7 +6,9 @@ const { hook_plan } = require('./hook_lib');
 const field_lib = require('./field_lib');
 const ExcelJS = require('exceljs');
 const uuid = require('uuid');
-const new_zczh = require('../plugin/new_zczh')
+const new_zczh = require('../plugin/new_zczh');
+const safe_check_module = require('../module/safe_check_module');
+const sc_lib = require('./sc_lib');
 
 module.exports = {
     fetch_vehicle: async function (_plate, _is_behind) {
@@ -840,26 +842,9 @@ module.exports = {
         ret.total = count;
         delete search_cond.offset
         delete search_cond.limit
-        // 已过期的安检项
-        let expired_sc=null;
         for (let index = 0; index < ret.reqs.length; index++) {
             let item = ret.reqs[index];
-            if (item.sc_content) {
-                expired_sc = await sq.models.sc_content.findOne({
-                    where: { id: item.sc_content.id },
-                });
-                if (expired_sc) {
-                    // 比较安检项有效期与当前日期时差，小于等于0则标记已过期状态
-                    expired_sc.passed = moment(moment(item.sc_content.expired_time)).diff(moment().format('YYYY-MM-DD'), 'days') > 0;;
-                    if (expired_sc.passed == false) {
-                        expired_sc.comment = '已过期';
-                    }else{
-                        expired_sc.comment = '';
-                    }
-                    await expired_sc.save();
-                }
-            }
-            
+            sc_lib.check_sc_content_expired(item.sc_content.id);
         }
         let first_one = await plan.stuff.getSc_reqs(search_cond);
         if (first_one.length == 0 || (first_one[0].sc_contents.length > 0 && first_one[0].sc_contents[0].passed)) {
