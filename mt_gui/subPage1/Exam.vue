1d1602e67408c3fb3d318ed4c84214601062bc61
marklion <marklion@sina.com>
2024-08-07 09:40:55 +0000

    ZYZL-396 #need more page

---
+++ mt_gui/subPage1/Exam.vue
@@ -0,0 +1,43 @@
+<template>
+<view>
+    <list-show :fetch_function="get_all_paper" :fetch_params="[plan_id]" height="40vh" v-model="all_paper">
+
+    </list-show>
+</view>
+</template>
+
+<script>
+import ListShow from '../components/ListShow.vue';
+export default {
+    name: 'Exam',
+    components:{
+        'list-show': ListShow
+    },
+    data: function () {
+        return {
+            plan_id: 0,
+            all_paper:[],
+        };
+    },
+    methods:{
+        get_all_paper:async function([plan_id]) {
+
+            let resp = await this.$send_req('/global/driver_get_paper', {
+
+            });
+            return resp.data;
+        },
+    },
+    onLoad: function (option) {
+        let plan_id = 0;
+        if (option.id) {
+            plan_id = parseInt(option.id);
+        }
+        this.plan_id = plan_id;
+    }
+}
+</script>
+
+<style>
+
+</style>
