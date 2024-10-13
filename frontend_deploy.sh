c14b13b7a4c4d9d975949ee9d4ecd295052f9f89
marklion <marklion@sina.com>
2024-10-13 11:21:07 +0800

    make frontend upload automatically

--- frontend_deploy.sh
+++ frontend_deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "frontend_deploy.sh"
+npm install -g miniprogram-ci
+pushd ./mt_gui/
+npm run build:mp-weixin
+popd
+pushd ./mt_gui/dist/build/
+VNAME=${1:1}
+miniprogram-ci upload --pp ./mp-weixin --pkp ../../../upload.key --uv ${VNAME} --appid wxfbf41c757510dc4c --use-project-config true -r 1
+popd
