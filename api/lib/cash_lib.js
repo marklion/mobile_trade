4958eb41a7cd85a879d786e41287a35c494c76b1
marklion <marklion@sina.com>
2025-03-17 11:04:09 +0800

    ZYZL-494 #add backend part of code

--- api/lib/cash_lib.js
+++ api/lib/cash_lib.js
@@ -4,6 +4,7 @@ const plan_lib = require('./plan_lib');
 const moment = require('moment');
 const ExcelJS = require('exceljs');
 const uuid = require('uuid');
+const { default: axios } = require('axios');
 module.exports = {
     charge: async function (_token, _contract_id, _cash_increased, _comment) {
         let user = await rbac_lib.get_user_by_token(_token);
@@ -131,6 +132,112 @@ module.exports = {
             await workbook.xlsx.writeFile('/database' + file_name);
             return file_name;
         }
-
+    },
+    req2u8c: async function (u8c_config, url, req) {
+        let ret = undefined;
+        try {
+            let resp = await axios.post(u8c_config.url + url, req, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    usercode: u8c_config.usercode,
+                    password: u8c_config.password,
+                    system: u8c_config.system_code,
+                },
+                httpsAgent: new httpsAgent({
+                    rejectUnauthorized: false,
+                    keepAlive: false,
+                })
+            });
+            if (resp.data.status == "success") {
+                ret = JSON.parse(resp.data.data);
+            }
+        } catch (error) {
+            console.log(error);
+            ret.err_msg = JSON.stringify(error);
+        }
+        return ret;
+    },
+    execute_u8c_oi:async function(u8c_oi) {
+        u8c_oi.is_running = true;
+        await u8c_oi.save();
+        setTimeout(async () => {
+            u8c_oi.is_running = false;
+            u8c_oi.run_log = moment().format('YYYY-MM-DD HH:mm:ss') + '执行成功';
+            await u8c_oi.save();
+        }, 30000);
+    },
+    create_u8c_record:async function(operator, plans, company) {
+        let new_u8c_oi = await db_opt.get_sq().models.u8c_order_info.create({
+            operator: operator,
+            time: moment().format('YYYY-MM-DD HH:mm:ss'),
+        });
+        for (let itr in plans) {
+            if (await itr.getU8c_order_info() == undefined) {
+                await itr.setU8c_order_info(new_u8c_oi);
+            }
+        }
+        if (await new_u8c_oi.countPlans() == 0) {
+            await new_u8c_oi.destroy();
+        }
+        else {
+            await new_u8c_oi.setCompany(company);
+            this.execute_u8c_oi();
+        }
+    },
+    sync2u8c: async function (operator, plans, company) {
+        let sale_plans = {};
+        let buy_plans = {};
+        plans.sort((a, b) => {
+            if (a.stuff.company.id < b.stuff.company.id) {
+                return -1;
+            }
+            if (a.stuff.company.id > b.stuff.company.id) {
+                return 1;
+            }
+            return 0;
+        });
+        plans.forEach(plan => {
+            let company = plan.stuff.company;
+            let plan_list = undefined;
+            if (plan.is_buy) {
+                plan_list = buy_plans[company.id];
+            }
+            else {
+                plan_list = sale_plans[company.id];
+            }
+            if (plan_list == undefined) {
+                plan_list = [];
+            }
+            plan_list.push(plan);
+            if (plan.is_buy) {
+                buy_plans[company.id] = plan_list;
+            }
+            else {
+                sale_plans[company.id] = plan_list;
+            }
+        });
+        for (const key in sale_plans) {
+            await this.create_u8c_record(operator, sale_plans[key], company);
+        }
+        for (const key in buy_plans) {
+            await this.create_u8c_record(operator, buy_plans[key], company);
+        }
+    },
+    get_u8c_oi: async function (token, pageNo) {
+        let company = await rbac_lib.get_company_by_token(token);
+        let ret = { count: 0, rows: [] };
+        if (company) {
+            ret.rows = await company.getU8c_order_infos({
+                offset: pageNo * 20,
+                limit: 20,
+                order: [['id', 'DESC']],
+                include: [{
+                    model: db_opt.get_sq().models.plan,
+                    include: [db_opt.get_sq().models.company]
+                }],
+            });
+            ret.count = await company.countU8c_order_infos();
+        }
+        return ret;
     },
 };
--- api/module/cash_module.js
+++ api/module/cash_module.js
@@ -1,5 +1,7 @@
 const cash_lib = require('../lib/cash_lib');
 const common = require('./common');
+const rbac_lib = require('../lib/rbac_lib');
+const { get_single_plan_by_id } = require('../lib/util_lib');
 module.exports = {
     name: 'cash',
     description: '余额管理',
@@ -52,7 +54,7 @@ module.exports = {
                 }
             },
         },
-        export_history:{
+        export_history: {
             name: '导出充值记录',
             description: '导出充值记录',
             is_write: false,
@@ -66,9 +68,76 @@ module.exports = {
                 result: { type: Boolean, mean: '结果', example: true }
             },
             func: async function (body, token) {
-                return await common.do_export_later(token, '余额明细', async ()=>{
+                return await common.do_export_later(token, '余额明细', async () => {
                     return await cash_lib.export_cash_history(token, body.contract_id, body.begin_time, body.end_time);
                 });
+            },
+        },
+        u8c_sync_order: {
+            name: '同步订单到U8C',
+            description: '同步订单到U8C',
+            is_write: true,
+            is_get_api: false,
+            params: {
+                plan_ids: {
+                    type: Array, have_to: true, mean: '计划ID数组', explain: {
+                        id: {
+                            type: Number, have_to: true, mean: '计划ID', example: 1
+                        }
+                    },
+                },
+            },
+            result: {
+                result: { type: Boolean, mean: '结果', example: true }
+            },
+            func: async function (body, token) {
+                let user = await rbac_lib.get_user_by_token(token);
+                let company = await rbac_lib.get_company_by_token(token);
+                let plans = [];
+                for (let plan_id in body.plan_ids) {
+                    let plan = await get_single_plan_by_id(plan_id);
+                    plans.push(plan);
+                }
+                await cash_lib.sync2u8c(user.name, plans, company);
+                return { result: true };
+            },
+        },
+        u8c_get_oi: {
+            name: '获取U8C订单同步信息',
+            description: '获取U8C订单同步信息',
+            is_write: false,
+            is_get_api: true,
+            params: {},
+            result: {
+                ois: {
+                    type: Array, mean: '订单同步信息', explain: {
+                        operator: { type: String, mean: '操作人', example: '张三' },
+                        time: { type: String, mean: '时间', example: '2020-03-01 12:00:00' },
+                        is_running: { type: Boolean, mean: '是否正在同步', example: true },
+                        run_log: { type: String, mean: '同步日志', example: '正在同步' },
+                        error_log: { type: String, mean: '错误日志', example: '错误' },
+                        plans: {
+                            type: Array, mean: '计划', explain: {
+                                id: { type: Number, mean: '计划ID', example: 1 },
+                                company: {
+                                    type: Object, mean: '公司', explain: {
+                                        id: { type: Number, mean: '公司ID', example: 1 },
+                                        name: { type: String, mean: '公司名', example: '公司1' }
+                                    }
+                                },
+                                plan_time: { type: String, mean: '计划时间', example: '2020-03-01 12:00:00' },
+                                count: { type: Number, mean: '装车量', example: 100 },
+                            },
+                        },
+                    }
+                },
+            },
+            func: async function (body, token) {
+                let resp = await cash_lib.get_u8c_oi(token, body.pageNo);
+                return {
+                    ois: resp.rows,
+                    total: resp.count,
+                }
             },
         },
     }
