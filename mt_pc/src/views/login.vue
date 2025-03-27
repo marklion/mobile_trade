b99a79c01767e8dd869f02e755aaa05ecde3d95d
marklion <marklion@sina.com>
2025-03-27 11:43:23 +0800

    ZYZL-503 #add prompt on login page

--- mt_pc/src/views/login.vue
+++ mt_pc/src/views/login.vue
@@ -14,7 +14,10 @@
             <span class="svg-container">
                 <svg-icon icon-class="password" />
             </span>
-            <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType" placeholder="密码" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
+            <el-popover placement="right" trigger="hover">
+                <img src="@/assets/login_prompt.jpg" style="width: 70%; height: 70%;">
+                <el-input slot="reference" :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType" placeholder="密码" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
+            </el-popover>
             <span class="show-pwd" @click="showPwd">
                 <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
             </span>
@@ -41,7 +44,7 @@ export default {
             }
         }
         return {
-            help_url:function() {
+            help_url: function () {
                 return process.env.REMOTE_HOST + '/help/'
             },
             loginForm: {
