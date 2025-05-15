fe5e3fd8a3a07507b6b8bf3ad993f8cca146f052
marklion <marklion@sina.com>
2025-05-15 16:26:10 +0800

    update for dup issue

--- mt_pc/src/views/u8c.vue
+++ mt_pc/src/views/u8c.vue
@@ -105,33 +105,7 @@ export default {
     data: function () {
         return {
             ois_selected: [],
-            pickerOptions: {
-                shortcuts: [{
-                    text: '最近一周',
-                    onClick(picker) {
-                        const end = new Date();
-                        const start = new Date();
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-                        picker.$emit('pick', [start, end]);
-                    }
-                }, {
-                    text: '最近一个月',
-                    onClick(picker) {
-                        const end = new Date();
-                        const start = new Date();
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-                        picker.$emit('pick', [start, end]);
-                    }
-                }, {
-                    text: '最近三个月',
-                    onClick(picker) {
-                        const end = new Date();
-                        const start = new Date();
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-                        picker.$emit('pick', [start, end]);
-                    }
-                }]
-            },
+            pickerOptions: this.$quik_date_option,
             filter: {
                 plan_time_start: '',
                 plan_time_end: '',
