907d2e47d88d080242f0c8f6232526460cfb3d4a
chenxiSolitude <chenxi_solitude@163.com>
2025-07-02 17:08:24 +0800

    ZYZL-567-现场门类型的设备增加可配置的操作选项 1.加一个switchAcc 2.v-if="company.access_control_permission && !device.scale_status"

--- mt_gui/src/subPage1/DevOpt.vue
+++ mt_gui/src/subPage1/DevOpt.vue
@@ -3,7 +3,7 @@
     <u-cell-group :title="device.name + '-' + device.scale_status">
         <u-cell>
             <view slot="label">
-                <u-row gutter="10" justify="space-between" v-if="company.access_control_permission">
+                <u-row gutter="10" justify="space-between" v-if="company.access_control_permission && !device.scale_status">
                     <u-col :span="3">
                         <u-button type="primary" size="mini" :text="'开' + gate_name.fg" @click="gate_ctrl(true, true)"></u-button>
                     </u-col>
