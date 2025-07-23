79d8ed9397e942fdccf646a2e26f630d6d293ef0
chenxiSolitude <chenxi_solitude@163.com>
2025-07-23 11:23:44 +0800

    ZYZL-578-余额明细导出记录名有错 调整余额明细导出记录名

--- api/module/cash_module.js
+++ api/module/cash_module.js
@@ -69,7 +69,14 @@ module.exports = {
                 result: { type: Boolean, mean: '结果', example: true }
             },
             func: async function (body, token) {
-                let company = await db_opt.get_sq().models.company.findByPk(body.contract_id);
+                let contract = await db_opt.get_sq().models.contract.findByPk(body.contract_id,{
+                    include:[{
+                        model:db_opt.get_sq().models.company,
+                        as:'buy_company',
+                    }],
+                });
+                let company = await db_opt.get_sq().models.company.findByPk(contract.saleCompanyId);
+
                 return await common.do_export_later(token, company.name + '的余额明细', async () => {
                     return await cash_lib.export_cash_history(token, body.contract_id, body.begin_time, body.end_time);
                 });
