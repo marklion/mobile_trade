6db77f2f8651930680ffc8d0bd28a54a47c8c39f
marklion <marklion@sina.com>
2025-11-12 12:59:34 +0800

    push again

--- api/lib/king_dee_sdk/src/getQueryStringFromParams.ts
+++ api/lib/king_dee_sdk/src/getQueryStringFromParams.ts
@@ -1,13 +1,19 @@
-const getQueryStringFromParams = function (params:{[key:string]:string}) {
-    let queryString = ''
-    for(let key in params){
-        if (queryString.length > 0) {
-            queryString = queryString + "&" + encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
-        } else {
-            queryString = encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
-        }
+const getQueryStringFromParams = function(params: { [key: string]: string }) {
+  let queryString = "";
+  for (let key in params) {
+    if (queryString.length > 0) {
+      queryString =
+        queryString +
+        "&" +
+        encodeURIComponent(key) +
+        "=" +
+        encodeURIComponent(params[key]);
+    } else {
+      queryString =
+        encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
     }
-    return queryString
-}
+  }
+  return queryString;
+};
 
-export default getQueryStringFromParams
+export default getQueryStringFromParams;
--- api/lib/king_dee_sdk/src/sha256AndBase64.ts
+++ api/lib/king_dee_sdk/src/sha256AndBase64.ts
@@ -1,9 +1,9 @@
-import { createHmac } from 'crypto'
+import { createHmac } from "crypto";
 
-const sha256AndBase64 = function (key:string, secret:string) {
-    let sha256 = createHmac('sha256', secret)
-    sha256.update(key)
-    return Buffer.from(sha256.digest('hex'), 'utf8').toString('base64')
-}
+const sha256AndBase64 = function(key: string, secret: string) {
+  let sha256 = createHmac("sha256", secret);
+  sha256.update(key);
+  return Buffer.from(sha256.digest("hex"), "utf8").toString("base64");
+};
 
-export default sha256AndBase64
+export default sha256AndBase64;
