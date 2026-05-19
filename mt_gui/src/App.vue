59ad2a31c5186c6d897e1d171123b545c63a77e9
chenxiSolitude <chenxi_solitude@163.com>
2026-05-19 13:45:24 +0800

    ZYZL-667-磅单和司机界面的新路由需要免登录 1.test again

--- mt_gui/src/App.vue
+++ mt_gui/src/App.vue
@@ -5,7 +5,7 @@ export default {
     },
     onShow: async function (options) {
         let path = options.path;
-        if (path == 'subPage/Ticket' || path == 'subPage/Driver') {
+        if (path == 'pages/Driver' || path == 'pages/Ticket' || path == 'subPage1/Ticket' || path == 'subPage1/Driver') {
             console.log('Driver or Ticket');
         } else {
             await this.$init_self();
