2166d31b3194316bc0a79b4e0dad1b9d717ad837
marklion <marklion@sina.com>
2026-08-04 15:57:00 +0800

    ZYZL-760 #make buy more sencse (#377)

--- api/module/tplus_module.js
+++ api/module/tplus_module.js
@@ -1,6 +1,8 @@
 const rbac_lib = require('../lib/rbac_lib');
 const t_plus_lib = require('../lib/t_plus_lib');
 const common = require('./common');
+const db_opt = require('../db_opt');
+const util_lib = require('../lib/util_lib');
 
 module.exports = {
     name: 'tplus',
@@ -151,6 +153,104 @@ module.exports = {
                     return await t_plus_lib.export_settle_detail(company, body.record_id);
                 });
             },
+        },
+        get_statistics_group_by_company: {
+            name: '按公司统计采购订单',
+            description: '按公司统计采购订单',
+            is_write: false,
+            is_get_api: false,
+            params: {
+                start_time: { type: String, have_to: true, mean: '开始出场时间', example: '2020-01-01 12:00:00' },
+                end_time: { type: String, have_to: true, mean: '结束出场时间', example: '2020-01-01 12:00:00' },
+                stuff_id: { type: Number, have_to: true, mean: '货物ID', example: 1 },
+            },
+            result: {
+                statistics: {
+                    type: Array, mean: '统计结果', explain: {
+                        company: {
+                            type: Object, mean: '公司', explain: {
+                                id: { type: Number, mean: '公司ID', example: 1 },
+                                name: { type: String, mean: '公司名称', example: '公司名称' },
+                            }
+                        },
+                        order_count: { type: Number, mean: '订单数', example: 1 },
+                    }
+                },
+            },
+            func: async function (body, token) {
+                let sq = db_opt.get_sq();
+                let ret = { statistics: [] };
+                let host_company = await rbac_lib.get_company_by_token(token);
+                let stuff = await sq.models.stuff.findByPk(body.stuff_id);
+                if (stuff && await host_company.hasStuff(stuff) && stuff.use_for_buy) {
+                    let ret_rows = await sq.models.plan.count({
+                        where: {
+                            stuffId: body.stuff_id,
+                            status: 3,
+                            p_time: {
+                                [db_opt.Op.gte]: body.start_time,
+                                [db_opt.Op.lt]: body.end_time,
+                            },
+                            is_buy: true,
+                            tplusSettleRecordId:null,
+                        },
+                        group: ['companyId'],
+                    })
+                    for (let row of ret_rows) {
+                        let company = await sq.models.company.findByPk(row.companyId);
+                        ret.statistics.push({
+                            company: {
+                                id: company.id,
+                                name: company.name,
+                            },
+                            order_count: row.count,
+                        })
+                    }
+                }
+                else {
+                    throw { err_msg: '货物不存在' }
+                }
+
+                return ret;
+            }
+        },
+        price_and_settle: {
+            name: '划价并结算',
+            description: '划价并结算',
+            is_write: true,
+            is_get_api: false,
+            params: {
+                start_time: { type: String, have_to: true, mean: '开始出场时间', example: '2020-01-01 12:00:00' },
+                end_time: { type: String, have_to: true, mean: '结束出场时间', example: '2020-01-01 12:00:00' },
+                stuff_id: { type: Number, have_to: true, mean: '货物ID', example: 1 },
+                company_id: { type: Number, have_to: true, mean: '公司ID', example: 1 },
+                price: { type: Number, have_to: true, mean: '价格', example: 100.5 },
+            },
+            result: {
+                result: { type: Boolean, mean: '结果', example: true },
+            },
+            func: async function (body, token) {
+                let sq = db_opt.get_sq();
+                let company = await sq.models.company.findByPk(body.company_id);
+                let orders = await company.getPlans({
+                    where: {
+                        p_time: {
+                            [db_opt.Op.gte]: body.start_time,
+                            [db_opt.Op.lt]: body.end_time,
+                        },
+                        stuffId: body.stuff_id,
+                        status: 3,
+                        is_buy: true,
+                    },
+                    include: util_lib.plan_detail_include(),
+                });
+                for (let order of orders) {
+                    order.unit_price = body.price;
+                    await order.save();
+                }
+                await t_plus_lib.do_settle(await rbac_lib.get_company_by_token(token), true, (await rbac_lib.get_user_by_token(token)).name, orders);
+                return { result: true };
+            }
         },
     },
 };
